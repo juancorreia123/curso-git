c332e7fb0a4468b5eed3435c3c2949b6955ba7f2
Juan Correia <juancorreia24@gmail.com>
2020-11-10 11:18:06 +0000

    Projeto iniciado, mostrado a mensagem Ola Mundo

---
+++ projecto teste/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecto_teste {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("olá Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
